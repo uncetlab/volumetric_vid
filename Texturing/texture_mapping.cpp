f1203f1a4bd966b2eee947445a6c1f6bd8511ab5
mhudnell <maxhudnell@gmail.com>
2020-01-14 13:53:00 -0500

    full texturemesh ex for single frame,   - added func to load cameras from json

--- Texturing/texture_mapping.cpp
+++ Texturing/texture_mapping.cpp
@@ -4,6 +4,18 @@
 #include <pcl/TextureMesh.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/features/normal_3d.h>
+#include <boost/filesystem.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <stdio.h>
+
+namespace pt = boost::property_tree;
+
+// helper function to retrieve an element at a specific index within a ptree
+template <typename T>
+T element_at(pt::ptree const& pt, std::string name, size_t n) {
+	return std::next(pt.get_child(name).find(""), n)->second.get_value<T>();
+}
 
 /** \brief Display a 3D representation showing the a cloud and a list of camera with their 6DOf poses */
 void showCameras(pcl::texture_mapping::CameraVector cams, pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud)
@@ -14,25 +26,40 @@ void showCameras(pcl::texture_mapping::CameraVector cams, pcl::PointCloud<pcl::P
 
 	// add a visual for each camera at the correct pose
 	for (int i = 0; i < cams.size(); ++i)
+	//for (int i = 0; i < 1; ++i)
 	{
 		// read current camera
 		pcl::TextureMapping<pcl::PointXYZ>::Camera cam = cams[i];
 		double focal = cam.focal_length;
+		double focal_h = cam.focal_length_h;
+		double focal_w = cam.focal_length_w;
 		double height = cam.height;
 		double width = cam.width;
 
 		float scale = 50.0;
+		//float scale = 1.0;
 		// create a 5-point visual for each camera
 		pcl::PointXYZ p1, p2, p3, p4, p5;
 		p1.x = 0; p1.y = 0; p1.z = 0;
-		double angleX = RAD2DEG(2.0 * atan(width / (2.0*focal)));
-		double angleY = RAD2DEG(2.0 * atan(height / (2.0*focal)));
 		double dist = 0.75;
+		double angleX, angleY;
 		double minX, minY, maxX, maxY;
-		maxX = dist * tan(atan(width / (2.0*focal)));
+
+		if (focal_h != -1 & focal_w != -1) {
+			maxX = dist * tan(atan(width / (2.0*focal_w)));
+			maxY = dist * tan(atan(height / (2.0*focal_h)));
+			angleX = RAD2DEG(2.0 * atan(width / (2.0*focal_w)));
+			angleY = RAD2DEG(2.0 * atan(height / (2.0*focal_h)));
+		}
+		else {
+			maxX = dist * tan(atan(width / (2.0*focal)));
+			maxY = dist * tan(atan(height / (2.0*focal)));
+			angleX = RAD2DEG(2.0 * atan(width / (2.0*focal)));
+			angleY = RAD2DEG(2.0 * atan(height / (2.0*focal)));
+		}
 		minX = -maxX;
-		maxY = dist * tan(atan(height / (2.0*focal)));
 		minY = -maxY;
+		
 		//p2.x = minX; p2.y = minY; p2.z = dist;
 		//p3.x = maxX; p3.y = minY; p3.z = dist;
 		//p4.x = maxX; p4.y = maxY; p4.z = dist;
@@ -90,6 +117,113 @@ void showCameras(pcl::texture_mapping::CameraVector cams, pcl::PointCloud<pcl::P
 	visu.spin();
 }
 
+void loadCameraParams(std::string calibration_file, pcl::texture_mapping::CameraVector &cam_list) {
+	pt::ptree root;
+	pt::read_json(calibration_file, root);
+
+	for (pt::ptree::value_type &cam : root.get_child("cameras"))
+	{
+		pt::ptree cam_tree = cam.second;
+		std::string type = cam_tree.get<std::string>("type");
+		std::string name = cam_tree.get<std::string>("name");
+
+
+		if (type != "kinect-color" | name != "50_02") {
+		//if (type != "kinect-color") {
+			continue;
+		}
+
+		//printf("\nname: %s\n", name.c_str());
+
+		// load rotation matrix R from json
+		Eigen::Matrix3f R;
+		int x = 0;
+		for (pt::ptree::value_type &row : cam_tree.get_child("R")) {
+			int y = 0;
+			for (pt::ptree::value_type &cell : row.second) {
+				//printf("%f ", cell.second.get_value<float>());
+				R(x, y) = cell.second.get_value<float>();
+				y++;
+			}
+			//printf("\n");
+			x++;
+		}
+
+		// load translation vector t from json
+		Eigen::Vector3f t;
+		int i = 0;
+		for (pt::ptree::value_type &row : cam_tree.get_child("t")) {
+			for (pt::ptree::value_type &cell : row.second) {
+				//printf("%f ", cell.second.get_value<float>());
+				t(i) = cell.second.get_value<float>();
+			}
+			i++;
+		}
+		//printf("\n");
+
+		// load intrinsic matrix K from json
+		Eigen::Matrix3f K;
+		x = 0;
+		for (pt::ptree::value_type &row : cam_tree.get_child("K")) {
+			int y = 0;
+			for (pt::ptree::value_type &cell : row.second) {
+				//printf("%f ", cell.second.get_value<float>());
+				K(x, y) = cell.second.get_value<float>();
+				y++;
+			}
+			//printf("\n");
+			x++;
+		}
+
+		Eigen::Matrix3f orientation = R.transpose();
+		Eigen::Vector3f location = -orientation*t;
+		//std::cout << "R^T =\n" << orientation << std::endl;
+		//std::cout << "-R^T*t =\n" << location << std::endl;
+
+		// set Camera variables
+		pcl::TextureMapping<pcl::PointXYZ>::Camera cam;
+		// location
+		cam.pose(0, 3) = location(0); //TX
+		cam.pose(1, 3) = location(1); //TY
+		cam.pose(2, 3) = location(2); //TZ
+
+		// orientation
+		cam.pose(0, 0) = orientation(0, 0);
+		cam.pose(0, 1) = orientation(0, 1);
+		cam.pose(0, 2) = orientation(0, 2);
+		cam.pose(1, 0) = orientation(1, 0);
+		cam.pose(1, 1) = orientation(1, 1);
+		cam.pose(1, 2) = orientation(1, 2);
+		cam.pose(2, 0) = orientation(2, 0);
+		cam.pose(2, 1) = orientation(2, 1);
+		cam.pose(2, 2) = orientation(2, 2);
+
+		// scale
+		cam.pose(3, 0) = 0.0;
+		cam.pose(3, 1) = 0.0;
+		cam.pose(3, 2) = 0.0;
+		cam.pose(3, 3) = 1.0;
+
+		// load cam resolution from json
+		cam.height = element_at<double>(cam_tree, "resolution", 1);
+		cam.width = element_at<double>(cam_tree, "resolution", 0);
+		cam.focal_length_h = K(1, 1);
+		cam.focal_length_w = K(0, 0);
+		cam.center_h = K(1, 2);
+		cam.center_w = K(0, 2);
+
+		// texture
+		// determine texture path -- for all images? just 380 for now
+		boost::filesystem::path calibration_path = calibration_file;
+		boost::filesystem::path texture_path = calibration_path.parent_path() / "kinectImgs" / name / (name + "_00000380.jpg");
+
+		cam.texture_file = texture_path.string();
+		//cam.texture_file = "C:/Users/maxhu/etlab/volumetric_capture/panoptic-toolbox/171026_pose3/kinectImgs/50_04/50_04_00000380.jpg";
+
+		cam_list.push_back(cam);
+	}
+}
+
 int main(int argc, char** argv) {
 
 	// ==============================================================================================
@@ -124,69 +258,51 @@ int main(int argc, char** argv) {
 	// Load textures and cameras poses and intrinsics
 	PCL_INFO("\nLoading textures and camera poses...\n");
 	pcl::texture_mapping::CameraVector my_cams;
+	loadCameraParams("C:/Users/maxhu/etlab/volumetric_capture/panoptic-toolbox/171026_pose3/calibration_171026_pose3.json", my_cams);
 
-	// TODO: automate camera loading like in example code below
-	//const boost::filesystem::path base_dir(".");
-	//std::string extension(".txt");
-	//int cpt_cam = 0;
-	//for (boost::filesystem::directory_iterator it(base_dir); it != boost::filesystem::directory_iterator(); ++it)
-	//{
-	//	if (boost::filesystem::is_regular_file(it->status()) && boost::filesystem::extension(it->path()) == extension)
-	//	{
-	//		pcl::TextureMapping<pcl::PointXYZ>::Camera cam;
-	//		readCamPoseFile(it->path().string(), cam);
-	//		cam.texture_file = boost::filesystem::basename(it->path()) + ".png";
-	//		my_cams.push_back(cam);
-	//		cpt_cam++;
-	//	}
-	//}
+	////// manually set one camera
+	//pcl::TextureMapping<pcl::PointXYZ>::Camera cam;
+	//cam.texture_file = "C:/Users/maxhu/etlab/volumetric_capture/panoptic-toolbox/171026_pose3/kinectImgs/50_04/50_04_00000380.jpg";
 
-	// manually set one camera
-	pcl::TextureMapping<pcl::PointXYZ>::Camera cam;
-	cam.texture_file = "C:/Users/maxhu/etlab/volumetric_capture/panoptic-toolbox/171026_pose3/kinectImgs/50_04/50_04_00000380.jpg";
+	//// location
+	//cam.pose(0, 3) = 224.20499997; //TX
+	//cam.pose(1, 3) = -104.32799999; //TY
+	//cam.pose(2, 3) = 141.55799999; //TZ
 
-	// location
-	//cam.pose(0, 3) = -233.7954; //TX
-	//cam.pose(1, 3) = -115.6553; //TY
-	//cam.pose(2, 3) = 114.6928; //TZ
+	//// orientation
+	//cam.pose(0, 0) = -0.4791;
+	//cam.pose(0, 1) = -0.0565;
+	//cam.pose(0, 2) = -0.8759;
+	//cam.pose(1, 0) = -0.0122;
+	//cam.pose(1, 1) = 0.9983;
+	//cam.pose(1, 2) = -0.0578;
+	//cam.pose(2, 0) = 0.8777;
+	//cam.pose(2, 1) = -0.0170;
+	//cam.pose(2, 2) = -0.4790;
 
-	cam.pose(0, 3) = 224.20499997; //TX
-	cam.pose(1, 3) = -104.32799999; //TY
-	cam.pose(2, 3) = 141.55799999; //TZ
+	//// scale
+	//cam.pose(3, 0) = 0.0;
+	//cam.pose(3, 1) = 0.0;
+	//cam.pose(3, 2) = 0.0;
+	//cam.pose(3, 3) = 1.0;
 
-	// orientation
-	cam.pose(0, 0) = -0.4791;
-	cam.pose(0, 1) = -0.0565;
-	cam.pose(0, 2) = -0.8759;
-	cam.pose(1, 0) = -0.0122;
-	cam.pose(1, 1) = 0.9983;
-	cam.pose(1, 2) = -0.0578;
-	cam.pose(2, 0) = 0.8777;
-	cam.pose(2, 1) = -0.0170;
-	cam.pose(2, 2) = -0.4790;
+	//// focal length, camera resolution, camera center (aka principle point offset)
+	//cam.focal_length = 1052.37;
+	////cam.focal_length_h = 1052.37;
+	////cam.focal_length_w = 1052.37;
+	//cam.height = 1080;
+	//cam.width = 1920;
+	//cam.center_h = 535.367;
+	//cam.center_w = 953.062;
 
-	// scale
-	cam.pose(3, 0) = 0.0;
-	cam.pose(3, 1) = 0.0;
-	cam.pose(3, 2) = 0.0;
-	cam.pose(3, 3) = 1.0;
-
-	// focal length, camera resolution, camera center (aka principle point offset)
-	cam.focal_length = 1052.37;
-	cam.height = 1080;
-	cam.width = 1920;
-	cam.center_h = 535.367;
-	cam.center_w = 953.062;
-
-	
-	my_cams.push_back(cam);
+	//my_cams.push_back(cam);
 
 	PCL_INFO("\tLoaded %d textures.\n", my_cams.size());
 	PCL_INFO("...Done.\n");
 
 	// Display cameras to user
 	PCL_INFO("\nDisplaying cameras. Press \'q\' to continue texture mapping\n");
-	//showCameras(my_cams, cloud);
+	showCameras(my_cams, cloud);
 
 	// ==============================================================================================
 	// Create materials for each texture (and one extra for occluded faces)
@@ -257,5 +373,5 @@ int main(int argc, char** argv) {
 	PCL_INFO("\nSaving mesh to textured_mesh.obj\n");
 
 	// MUST be declared with forward slashes to work correctly
-	pcl::io::saveOBJFile("C:/Users/maxhu/Desktop/uvatlas_example/test_texturing.obj", tmesh);
+	pcl::io::saveOBJFile("C:/Users/maxhu/Desktop/uvatlas_example/test_texturing_50_02.obj", tmesh);
 }
