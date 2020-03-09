786ea0be2b6d44e83ce1edb24b0617cef6652669
mhudnell <mhudnell@cs.unc.edu>
2020-03-09 18:54:38 -0400

    refactor usd demo

--- demos/demo_usd.cpp
+++ demos/demo_usd.cpp
@@ -9,11 +9,127 @@
 #include <pxr/usd/sdf/layer.h>
 #include <pxr/base/vt/array.h>
 
+void build_cube(
+	pxr::VtVec3fArray &pts, pxr::VtVec3fArray &extentArray, pxr::VtArray<int> &faceVertexCounts, pxr::VtArray<int> &faceVertexIndices
+) {
+
+	//======= build points for cube!
+	pts.reserve(8);
+	pts.push_back(pxr::GfVec3f(-50, 0, 50));	// 0
+	pts.push_back(pxr::GfVec3f(50, 0, 50));		// 1
+	pts.push_back(pxr::GfVec3f(50, 100, 50));	// 2
+	pts.push_back(pxr::GfVec3f(-50, 100, 50));	// 3
+	pts.push_back(pxr::GfVec3f(-50, 0, -50));	// 4
+	pts.push_back(pxr::GfVec3f(50, 0, -50));	// 5
+	pts.push_back(pxr::GfVec3f(50, 100, -50));	// 6
+	pts.push_back(pxr::GfVec3f(-50, 100, -50));	// 7
+
+	//====== calculate extent
+	pxr::GfRange3f extent;
+	for (const auto& pt : pts) {
+		extent.UnionWith(pt);
+	}
+	extentArray[0] = extent.GetMin();
+	extentArray[1] = extent.GetMax();
+
+	//======= build faces
+
+	// face 1
+	faceVertexIndices.push_back(0);
+	faceVertexIndices.push_back(1);
+	faceVertexIndices.push_back(2);
+	faceVertexIndices.push_back(3);
+	faceVertexCounts.push_back(4);
+
+	// face 2
+	faceVertexIndices.push_back(0);
+	faceVertexIndices.push_back(3);
+	faceVertexIndices.push_back(7);
+	faceVertexIndices.push_back(4);
+	faceVertexCounts.push_back(4);
+
+	// face 3
+	faceVertexIndices.push_back(3);
+	faceVertexIndices.push_back(2);
+	faceVertexIndices.push_back(6);
+	faceVertexIndices.push_back(7);
+	faceVertexCounts.push_back(4);
+
+	// face 4
+	faceVertexIndices.push_back(2);
+	faceVertexIndices.push_back(1);
+	faceVertexIndices.push_back(5);
+	faceVertexIndices.push_back(6);
+	faceVertexCounts.push_back(4);
+
+	// face 5
+	faceVertexIndices.push_back(1);
+	faceVertexIndices.push_back(0);
+	faceVertexIndices.push_back(4);
+	faceVertexIndices.push_back(5);
+	faceVertexCounts.push_back(4);
+
+	// face 6
+	faceVertexIndices.push_back(4);
+	faceVertexIndices.push_back(5);
+	faceVertexIndices.push_back(6);
+	faceVertexIndices.push_back(7);
+	faceVertexCounts.push_back(4);
+}
+
+void build_pyramid(
+	pxr::VtVec3fArray &pts, pxr::VtVec3fArray &extentArray, pxr::VtArray<int> &faceVertexCounts, pxr::VtArray<int> &faceVertexIndices
+) {
+	//======= build points for pyramid!
+
+	pts.reserve(4);
+	pts.push_back(pxr::GfVec3f(0, 0, 0));		// 0
+	pts.push_back(pxr::GfVec3f(50, 0, 0));		// 1
+	pts.push_back(pxr::GfVec3f(25, 50, 0));		// 2
+	pts.push_back(pxr::GfVec3f(25, 25, 50));	// 3
+
+	// Usd currently requires an extent, somewhat unfortunately.
+	pxr::GfRange3f extent1;
+	for (const auto& pt : pts) {
+		extent1.UnionWith(pt);
+	}
+	extentArray[0] = extent1.GetMin();
+	extentArray[1] = extent1.GetMax();
+
+	//======= build faces
+
+	// face 1
+	faceVertexIndices.push_back(0);
+	faceVertexIndices.push_back(1);
+	faceVertexIndices.push_back(2);
+	faceVertexCounts.push_back(3);
+
+	// face 2
+	faceVertexIndices.push_back(0);
+	faceVertexIndices.push_back(3);
+	faceVertexIndices.push_back(2);
+	faceVertexCounts.push_back(3);
+
+	// face 3
+	faceVertexIndices.push_back(1);
+	faceVertexIndices.push_back(3);
+	faceVertexIndices.push_back(2);
+	faceVertexCounts.push_back(3);
+
+	// face 4
+	faceVertexIndices.push_back(2);
+	faceVertexIndices.push_back(3);
+	faceVertexIndices.push_back(0);
+	faceVertexCounts.push_back(3);
+
+}
 
 void create_seq() {
 	auto stage = pxr::UsdStage::CreateNew("HelloMesh.usda");
-	stage->SetStartTimeCode(0);
-	stage->SetEndTimeCode(48);
+	int start_time = 0;
+	int half_time = 24;
+	stage->SetStartTimeCode(start_time);
+	stage->SetEndTimeCode(half_time*2);
 
 	auto usdMesh = pxr::UsdGeomMesh::Define(stage, pxr::SdfPath("/hello/mesh"));
 
@@ -21,75 +137,10 @@ void create_seq() {
 	//         TIME SAMPLE 1: CUBE
 	//================================
 
-	//======= build points for cube!
 	pxr::VtVec3fArray usdPoints0;
-
-	usdPoints0.reserve(8);
-	usdPoints0.push_back(pxr::GfVec3f(-50, 0, 50));		// 0
-	usdPoints0.push_back(pxr::GfVec3f(50, 0, 50));		// 1
-	usdPoints0.push_back(pxr::GfVec3f(50, 100, 50));	// 2
-	usdPoints0.push_back(pxr::GfVec3f(-50, 100, 50));	// 3
-	usdPoints0.push_back(pxr::GfVec3f(-50, 0, -50));	// 4
-	usdPoints0.push_back(pxr::GfVec3f(50, 0, -50));		// 5
-	usdPoints0.push_back(pxr::GfVec3f(50, 100, -50));	// 6
-	usdPoints0.push_back(pxr::GfVec3f(-50, 100, -50));	// 7
-
-	// Usd currently requires an extent, somewhat unfortunately.
-	pxr::GfRange3f extent0;
-	for (const auto& pt : usdPoints0) {
-		extent0.UnionWith(pt);
-	}
 	pxr::VtVec3fArray extentArray0(2);
-	extentArray0[0] = extent0.GetMin();
-	extentArray0[1] = extent0.GetMax();
-
-	//======= build faces
-
-	//store a vector with number of sides per polygon
-	//in the test model there are only triangles so this is going to be 3, 3, 3, ... for the number of polygons
 	pxr::VtArray<int> faceVertexCounts0, faceVertexIndices0;
-
-	// face 1
-	faceVertexIndices0.push_back(0);
-	faceVertexIndices0.push_back(1);
-	faceVertexIndices0.push_back(2);
-	faceVertexIndices0.push_back(3);
-	faceVertexCounts0.push_back(4);
-
-	// face 2
-	faceVertexIndices0.push_back(0);
-	faceVertexIndices0.push_back(3);
-	faceVertexIndices0.push_back(7);
-	faceVertexIndices0.push_back(4);
-	faceVertexCounts0.push_back(4);
-
-	// face 3
-	faceVertexIndices0.push_back(3);
-	faceVertexIndices0.push_back(2);
-	faceVertexIndices0.push_back(6);
-	faceVertexIndices0.push_back(7);
-	faceVertexCounts0.push_back(4);
-
-	// face 4
-	faceVertexIndices0.push_back(2);
-	faceVertexIndices0.push_back(1);
-	faceVertexIndices0.push_back(5);
-	faceVertexIndices0.push_back(6);
-	faceVertexCounts0.push_back(4);
-
-	// face 5
-	faceVertexIndices0.push_back(1);
-	faceVertexIndices0.push_back(0);
-	faceVertexIndices0.push_back(4);
-	faceVertexIndices0.push_back(5);
-	faceVertexCounts0.push_back(4);
-
-	// face 6
-	faceVertexIndices0.push_back(4);
-	faceVertexIndices0.push_back(5);
-	faceVertexIndices0.push_back(6);
-	faceVertexIndices0.push_back(7);
-	faceVertexCounts0.push_back(4);
+	build_cube(usdPoints0, extentArray0, faceVertexCounts0, faceVertexIndices0);
 
 	//================================
 	//         TIME SAMPLE 2: PYRAMID
@@ -97,59 +148,14 @@ void create_seq() {
 
 	//======= build points for cube!
 	pxr::VtVec3fArray usdPoints1;
-
-	usdPoints1.reserve(4);
-	usdPoints1.push_back(pxr::GfVec3f(0, 0, 0));		// 0
-	usdPoints1.push_back(pxr::GfVec3f(50, 0, 0));		// 1
-	usdPoints1.push_back(pxr::GfVec3f(25, 50, 0));		// 2
-	usdPoints1.push_back(pxr::GfVec3f(25, 25, 50));		// 3
-
-	// Usd currently requires an extent, somewhat unfortunately.
-	pxr::GfRange3f extent1;
-	for (const auto& pt : usdPoints1) {
-		extent1.UnionWith(pt);
-	}
 	pxr::VtVec3fArray extentArray1(2);
-	extentArray1[0] = extent1.GetMin();
-	extentArray1[1] = extent1.GetMax();
-
-	//======= build faces
-
-	//store a vector with number of sides per polygon
-	//in the test model there are only triangles so this is going to be 3, 3, 3, ... for the number of polygons
 	pxr::VtArray<int> faceVertexCounts1, faceVertexIndices1;
-
-	// face 1
-	faceVertexIndices1.push_back(0);
-	faceVertexIndices1.push_back(1);
-	faceVertexIndices1.push_back(2);
-	faceVertexCounts1.push_back(3);
-
-	// face 2
-	faceVertexIndices1.push_back(0);
-	faceVertexIndices1.push_back(3);
-	faceVertexIndices1.push_back(2);
-	faceVertexCounts1.push_back(3);
-
-	// face 3
-	faceVertexIndices1.push_back(1);
-	faceVertexIndices1.push_back(3);
-	faceVertexIndices1.push_back(2);
-	faceVertexCounts1.push_back(3);
-
-	// face 4
-	faceVertexIndices1.push_back(2);
-	faceVertexIndices1.push_back(3);
-	faceVertexIndices1.push_back(0);
-	faceVertexCounts1.push_back(3);
+	build_pyramid(usdPoints1, extentArray1, faceVertexCounts1, faceVertexIndices1);
 
 	//================================
 	//     SET THE TIME-SAMPLED ATTRS
 	//================================
-	int start_time = 0;
-	int half_time = 24;
-
-	//usdMesh.GetPointsAttr().GetTimeSamples()
+	
 	usdMesh.GetPointsAttr().Set(usdPoints0, start_time);
 	usdMesh.GetPointsAttr().Set(usdPoints1, half_time);
 
