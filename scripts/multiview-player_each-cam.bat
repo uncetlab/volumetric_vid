801bc612a8947fd556e2d51e1209f56f48b0d2f8
mhudnell <mhudnell@cs.unc.edu>
2020-05-01 16:50:10 -0400

    add simple script for processing unsynced vcl recordings

---
+++ scripts/multiview-player_each-cam.bat
@@ -0,0 +1,6 @@
+set out_dir=%1
+
+D:\mhudnell\programs\VCL3D\3.0\multiview_player\multiview-player.exe -f d4150.cdv -o %out_dir% --pcloud --is_pcloud_calibrated --color --show_progress --force_creation
+D:\mhudnell\programs\VCL3D\3.0\multiview_player\multiview-player.exe -f d4151.cdv -o %out_dir% --pcloud --is_pcloud_calibrated --color --show_progress --force_creation
+D:\mhudnell\programs\VCL3D\3.0\multiview_player\multiview-player.exe -f d4152.cdv -o %out_dir% --pcloud --is_pcloud_calibrated --color --show_progress --force_creation
+D:\mhudnell\programs\VCL3D\3.0\multiview_player\multiview-player.exe -f d4153.cdv -o %out_dir% --pcloud --is_pcloud_calibrated --color --show_progress --force_creation
